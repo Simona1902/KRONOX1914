05342a177e625ddcca58aca2025b84ff36e878ce
Simona <simonamarinacci04@gmail.com>
2026-03-02 18:04:18 +0100

    Add files via upload

---
+++ adventure/src/main/java/constants/GameIDs.java
@@ -0,0 +1,348 @@
+package constants;
+/**
+ * Classe di utilità che contiene gli ID statici e finali per tutti 
+ * gli oggetti e le stanze del gioco.
+ * Questo evita l'uso di "Magic Numbers" nel codice, rendendolo più leggibile e sicuro.
+ * 
+ * @author simona
+ */
+public final class GameIDs {
+
+    private GameIDs() {}
+
+    // --- OGGETTI PRESENTI ---
+
+    /**
+     * 
+     */
+    public static final int LETTERA_PROFESSORE = 100;
+
+    /**
+     *
+     */
+    public static final int CHIAVE_MAGAZZINO = 102;
+
+    /**
+     *
+     */
+    public static final int PENDRIVE_KRONOX = 103;
+
+    /**
+     *
+     */
+    public static final int FOGLIO_INDOVINELLO = 104;
+
+    /**
+     *
+     */
+    public static final int TERMINALE_KRONOX = 105;
+
+    /**
+     *
+     */
+    public static final int CATTEDRA_PROFESSORE = 106;
+
+    /**
+     *
+     */
+    public static final int CASSETTO = 107;
+
+    /**
+     *
+     */
+    public static final int SCATOLA_ROSSA = 110;
+
+    /**
+     *
+     */
+    public static final int SCALA_LEGNO = 111;
+
+    /**
+     *
+     */
+    public static final int INTERRUTTORE_LUCE = 112;
+
+    /**
+     *
+     */
+    public static final int BOTOLA = 113;
+
+    /**
+     *
+     */
+    public static final int PORTA_MAGAZZINO = 114;
+
+    // --- OGGETTI PASSATO (1914) ---
+
+    /**
+     *
+     */
+    public static final int SEGRETARIO = 250;
+
+    /**
+     *
+     */
+    public static final int NUNZIO = 251;
+
+    /**
+     *
+     */
+    public static final int CRIPTEX = 253;
+
+    /**
+     *
+     */
+    public static final int QUADRO_SCHONBRUNN = 254;
+
+    /**
+     *
+     */
+    public static final int LEGGIO_UDIENZE = 255;
+
+    /**
+     *
+     */
+    public static final int TAVOLA_PRANZO = 256;
+
+    /**
+     *
+     */
+    public static final int CHIAVE_STRANA_CRIPTEX = 257;
+
+    /**
+     *
+     */
+    public static final int CAPPOTTO_NUNZIO = 258;
+
+    /**
+     *
+     */
+    public static final int CONFESSIONALE = 260;
+
+    /**
+     *
+     */
+    public static final int DOCUMENTI_CONFRATERNITA = 261;
+
+    /**
+     *
+     */
+    public static final int BOZZA_TELEGRAMMA = 262;
+
+    /**
+     *
+     */
+    public static final int STATUA_EQUESTRE = 263;
+
+    /**
+     *
+     */
+    public static final int ANELLO_NUNZIO = 264;
+
+    /**
+     *
+     */
+    public static final int LIBRO_SIGILLATO = 265;
+
+    /**
+     *
+     */
+    public static final int MAPPA_AMBASCIATA = 266;
+
+    /**
+     *
+     */
+    public static final int SCAFFALE_POLVEROSO = 267;
+
+    /**
+     *
+     */
+    public static final int TECHE_VUOTE = 269;
+
+    /**
+     *
+     */
+    public static final int OROLOGIO_PENDOLO = 270;
+
+    /**
+     *
+     */
+    public static final int PERGAMENA_CODICE = 271;
+
+    /**
+     *
+     */
+    public static final int ARMADIO = 272;
+
+    /**
+     *
+     */
+    public static final int LETTO_NUNZIO = 273;
+
+    /**
+     *
+     */
+    public static final int INGINOCCHIATOIO = 274;
+
+    /**
+     *
+     */
+    public static final int MACCHINA_SCRIVERE = 275;
+
+    /**
+     *
+     */
+    public static final int SCHEDARIO = 276;
+
+    /**
+     *
+     */
+    public static final int POLTRONA_OSPITI = 277;
+
+    /**
+     *
+     */
+    public static final int SCRIVANIA_NUNZIO = 278;
+
+    /**
+     *
+     */
+    public static final int PORTA_BLINDATA_ARCHIVIO = 279;
+
+    /**
+     *
+     */
+    public static final int POLTRONE_UDIENZE = 280;
+
+    /**
+     *
+     */
+    public static final int TOMBA_FONDATORE = 281;
+
+    // --- STANZE PRESENTE ---
+
+    /**
+     *
+     */
+    public static final int UFFICIO_ASSISTENTE_2025 = 1;
+
+    /**
+     *
+     */
+    public static final int UFFICIO_PROFESSORE_2025 = 2;
+
+    /**
+     *
+     */
+    public static final int CORRIDOIO_2025 = 3;
+
+    /**
+     *
+     */
+    public static final int BAGNO_2025 = 4;
+
+    /**
+     *
+     */
+    public static final int MAGAZZINO_2025 = 5;
+
+    /**
+     *
+     */
+    public static final int SCANTINATO_SEGRETO_2025 = 6;
+    
+    // --- STANZE PASSATO(1914) ---
+
+    /**
+     *
+     */
+    public static final int INGRESSO_AMBASCIATA_1914 = 200;
+
+    /**
+     *
+     */
+    public static final int SEGRETERIA_1914 = 201;
+
+    /**
+     *
+     */
+    public static final int STANZA_TESORO_1914 = 202;
+
+    /**
+     *
+     */
+    public static final int CORRIDOIO_ALA_OVEST_1914 = 203;
+
+    /**
+     *
+     */
+    public static final int SALA_DIPINTI_1914 = 204;
+
+    /**
+     *
+     */
+    public static final int SALA_UDIENZE_1914 = 205;
+
+    /**
+     *
+     */
+    public static final int SALA_PRANZO_1914 = 206;
+
+    /**
+     *
+     */
+    public static final int GIARDINO_1914 = 207;
+
+    /**
+     *
+     */
+    public static final int BIBLIOTECA_1914 = 208;
+
+    /**
+     *
+     */
+    public static final int UFFICIO_NUNZIO_1914 = 210;
+
+    /**
+     *
+     */
+    public static final int CAMERA_NUNZIO_1914 = 211;
+
+    /**
+     *
+     */
+    public static final int CAPPELLA_PERSONALE_1914 = 212;
+
+    /**
+     *
+     */
+    public static final int ARCHIVIO_SEGRETO_1914 = 213;
+
+    /**
+     *
+     */
+    public static final int BALCONE_1914 = 215;
+
+    /**
+     *
+     */
+    public static final int CRIPTA_SOTTERRANEA_1914 = 220;
+
+    /**
+     *
+     */
+    public static final int CAPPELLA_FUNEBRE_1914 = 222;
+    
+    // --- EPOCHE ---
+
+    /**
+     *
+     */
+    public static final String PRESENT_EPOCH = "presente";
+
+    /**
+     *
+     */
+    public static final String PAST_EPOCH = "passato";
+
+}
